c73a9fa8ce9f94ff653759d93f2662a32f3ebffb
eswar <bujjieshu@gmail.com>
2017-09-23 09:59:50 +0000

    changes for maven 3

---
+++ src/main/java/com/digital/project3/App.java
@@ -0,0 +1,13 @@
+package com.digital.project3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello nanna!" );
+    }
+}
